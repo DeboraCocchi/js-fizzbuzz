6a87ae94de0574f1f0c4ffdeca3f427732795cc0
DeboraCocchi <cccdbr@gmail.com>
2022-10-13 14:57:06 +0200

    [done]with-visual-squares

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 // .num multiplo solo di 5  Buzz
 // -num multipl di entrambi “FizzBuzz”
 // multiplo di nessuno (stampo il numero)
-const container = document.querySelector('container-fluid');
+const container = document.querySelector('.dc-container');
 
 for(i=1; i<=100; i++){
 
   const box = document.createElement('div');
   box.className='box';
+  
 
   if( !(i % 3) && !(i % 5)){
     console.log(i , 'multiplo di entrambi --- FizzBuzz');
     box.innerHTML='FizzBuzz';
-    box.classList.add('red');
+    box.classList.add('brown');
   } else if(!(i % 3) && (i % 5)){
     console.log(i , 'multiplo di 3 --- Fizz');
     box.innerHTML='Fizz';
@@ -29,5 +30,9 @@ for(i=1; i<=100; i++){
   }else{
     console.log(i);
     box.innerHTML=i;
+    box.classList.add('aqua');
   }
+
+  container.append(box);
 }
+
